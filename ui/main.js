527a493b7bcc97bfb8bfd48b9e7dab6376fabd4e
svenkat89 <svenkat89@gmail.com>
2016-10-06 14:30:59 +0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,12 +2,14 @@ console.log('Loaded!');
 var element = document.getElementById("image");
 var element1 = document.getElementById("main");
 element1.innerHTML='New World';
-var main = function(){
-    element.onclick(function() {
-        element.animate({
-            left:"10px"
-        },200);
-        });
-    };
+//var main = function(){
+//    element.onclick(function() {
+//        element.animate({
+//            left:"10px"
+//        },200);
+//        });
+//    };
     
-$(document).ready(main);
+element.onclick = function() {
+    img.style.marginRight="10px";
+};
