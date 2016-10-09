d5995e044ab7f5f313879541055529fc153d9c51
svenkat89 <svenkat89@gmail.com>
2016-10-09 17:01:42 +0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,14 +2,23 @@ console.log('Loaded!');
 var element = document.getElementById("image");
 var element1 = document.getElementById("main");
 element1.innerHTML='New World';
-//var main = function(){
-//    element.onclick(function() {
-//        element.animate({
-//            left:"10px"
-//        },200);
-//        });
-//    };
-    
 element.onclick = function() {
     element.style.marginRight='10px';
 };
+var element2 = document.getElementById("button");
+element.onclick = function() {
+  //Go to COunter 1 URL
+  var request= new XMLHttpRequest();
+  request.open('Get','http://svenkat89.imad.hasura-app.io/counter1',true);
+  
+  //Get response
+  request.onreadystatechange = function() {
+      if(request.readystate==XMLHttpRequest.Done){
+          if(request.status == 200){
+            var counter = counter*2;
+            var new1 = document.getElementById("count");
+            new1.innerHTML=counter.toString();
+          }
+      }
+  }
+  };
