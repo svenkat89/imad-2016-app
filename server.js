e6270ebd61efb1c1134ce64a4a18dc25d39b1d52
svenkat89 <svenkat89@gmail.com>
2016-10-11 20:20:48 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/article-1', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'arti-1.html'));
+});
 var counter=1;
 app.get('/counter1', function (req, res) {
   counter = counter*2;
