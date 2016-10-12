87bd13260cf4ab959f34cff4ebe80bee615b279d
svenkat89 <svenkat89@gmail.com>
2016-10-12 11:50:32 +0800

    [imad-console] Updates ui/articles.js

--- ui/articles.js
+++ ui/articles.js
@@ -1,1 +1,25 @@
+console.log('loaded');
+var element4 = document.getElementById('submitcomment');
 
+element4.onclick = function() {
+        //send name to server
+      var request = new XMLHttpRequest();
+      request.open('Get','http://svenkat89.imad.hasura-app.io/article-2',true);
+      request.send(null);
+      //render the response as a list
+      request.onreadystatechange = function() {
+      if(request.readystate === XMLHttpRequest.Done){
+          if(request.status === 200){
+            var commentlist='';
+            var comments = request.responseText;//storin the response
+            comments = JSON.parse(comments);//converting to array again from JSON
+            for(var i =0 ; i < comments.length ; i++){
+                commentlist+='<textrea>'+ comments[i] +'</textarea>';
+            }
+        var ul= document.getElementById('comment_section');
+        ul.innerHTML=commentlist;
+        }
+      }
+};
+        
+    };
