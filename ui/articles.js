4004d346b6193e52e7515343397fba8cd1aab933
svenkat89 <svenkat89@gmail.com>
2016-10-13 09:38:09 +0800

    [imad-console] Updates ui/articles.js

--- ui/articles.js
+++ ui/articles.js
@@ -2,28 +2,18 @@ console.log('loaded');
   //comments.push(commentvalue);
   //res.send(JSON.stringify(comments));
 var element4 = document.getElementById('submitcomment');
-
     element4.onclick = function() {
+        var comments=[];
         //console.log(document.getElementById('comment1').value);
-        alert(document.getElementById('comment1').value);
+        comments=(document.getElementById('comment1').value);
         //send name to server
-      var request = new XMLHttpRequest();
-      request.open('Get','http://svenkat89.imad.hasura-app.io/article-1',true);
-      request.send(null);
-      //render the response as a list
-      request.onreadystatechange = function() {
-      if(request.readystate === XMLHttpRequest.Done){
-          if(request.status === 200){
+        console.log(comments);
             var commentlist='';
-            var comments = request.responseText;//storin the response
-            comments = JSON.parse(comments);//converting to array again from JSON
+        
+            //comments = JSON.parse(comments);//converting to array again from JSON
             for(var i =0 ; i < comments.length ; i++){
                 commentlist+='<textrea>'+ comments[i] +'</textarea>';
             }
         var ul= document.getElementById('comment_section');
         ul.innerHTML=commentlist;
-        }
-      }
-};
-        
     };
