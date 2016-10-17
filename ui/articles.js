1397534b908126cd957c8afdc9e55b2a047504ea
svenkat89 <svenkat89@gmail.com>
2016-10-17 15:47:55 +0800

    [imad-console] Updates ui/articles.js

--- ui/articles.js
+++ ui/articles.js
@@ -2,14 +2,14 @@ console.log('loaded');
   //comments.push(commentvalue);
   //res.send(JSON.stringify(comments));
   var comments=[];
+  var valuecomment=(document.getElementById('comment1').value);
     var element4 = document.getElementById('submitcomment');
     element4.onclick = function() {
         //console.log(document.getElementById('comment1').value);
         var commentlist='';
-         var valuecomment=(document.getElementById('comment1').value);
          comments.push(valuecomment);
-         console.log(comments)
         for(var i =0 ; i < comments.length ; i++){
+            console.log(comments[i]);
             commentlist+='<li>'+ comments[i] +'</li>';
             }
         var ul= document.getElementById('comment_section');
